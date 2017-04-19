02099a39c384bc0a088a2b548cfb0408b337e230
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-19 21:39:27 +1000

    interaction testing for field

--- SuperExample/components/__tests__/Field.js
+++ SuperExample/components/__tests__/Field.js
@@ -15,3 +15,32 @@ test('Field', () => {
 
   expect(tree).toMatchSnapshot();
 });
+
+test('Field label', () => {
+  const tree = renderer.create(
+    <Field
+      updater={() => {}}
+      label="label"
+      value="value"
+    />,
+  ).toJSON();
+  const subject = tree.children.find(c => c.type === 'Text').children[0];
+
+  expect(subject).toEqual('label');
+});
+
+test('Field updater', () => {
+  const updater = jest.fn();
+  const tree = renderer.create(
+    <Field
+      updater={updater}
+      label="label"
+      value="value"
+    />,
+  ).toJSON();
+  const onChangeText = tree.children.find(c => c.type === 'TextInput').props.onChangeText;
+
+  onChangeText('bananas');
+
+  expect(updater).toBeCalledWith('bananas');
+});
