d386b4debdbd645d8f7e7f4d48f6db0cba20cbce
Alec Brooks <abrooks@thoughtworks.com>
2017-04-19 22:04:31 +1000

    Demonstrate View lack of scrolling

--- SuperExample/screens/Form.js
+++ SuperExample/screens/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 import Field from '../components/Field';
 
@@ -22,18 +22,68 @@ export default class extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <View>
+        <Text style={{ fontSize: 20, padding: 10 }}>
+          {this.nameEntered()
+            ? `Hello ${this.state.name}!`
+            : 'Hey there Chump!'}
+        </Text>
         <Field
           label="Please enter your name"
           updater={value => this.setState({ name: value })}
           value={this.state.name}
         />
-        <Text style={{ fontSize: 20 }}>
-          {this.nameEntered()
-            ? `Hello ${this.state.name}!`
-            : 'Hey there Chump!'}
-        </Text>
-      </ScrollView>
+        <Field
+          label="Please enter your number"
+          updater={value => this.setState({ number: value })}
+          value={this.state.number}
+        />
+        <Field
+          label="Please enter your address"
+          updater={value => this.setState({ address: value })}
+          value={this.state.address}
+        />
+        <Field
+          label="Please enter your email"
+          updater={value => this.setState({ email: value })}
+          value={this.state.email}
+        />
+        <Field
+          label="Please enter your post code"
+          updater={value => this.setState({ postCode: value })}
+          value={this.state.postCode}
+        />
+        <Field
+          label="Please enter your dad's name"
+          updater={value => this.setState({ dadsName: value })}
+          value={this.state.dadsName}
+        />
+        <Field
+          label="Please enter your dad's dad's name"
+          updater={value => this.setState({ dadsDadsName: value })}
+          value={this.state.dadsDadsName}
+        />
+        <Field
+          label="Please enter your dad's dad's dad's name"
+          updater={value => this.setState({ dadsDadsDadsName: value })}
+          value={this.state.dadsDadsDadsName}
+        />
+        <Field
+          label="Please enter your dad's dad's dad's dad's dad's name"
+          updater={value => this.setState({ dadsDadsDadsDadsName: value })}
+          value={this.state.dadsDadsDadsDadsName}
+        />
+        <Field
+          label="Please enter your dad's dad's dad's dad's dad's dad's name"
+          updater={value => this.setState({ dadsDadsDadsDadsDadsName: value })}
+          value={this.state.dadsDadsDadsDadsDadsName}
+        />
+        <Field
+          label="Please enter your dad's dad's dad's dad's dad's dad's dad's name"
+          updater={value => this.setState({ dadsDadsDadsDadsDadsDadsName: value })}
+          value={this.state.dadsDadsDadsDadsDadsDadsName}
+        />
+      </View>
     );
   }
 }
