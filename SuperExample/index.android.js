e2c9c0d7d17e1bc7a27a9f569919f13337dcacc9
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-19 19:01:37 +1000

    Restructured app to be multiplatform

--- SuperExample/index.android.js
+++ SuperExample/index.android.js
@@ -12,42 +12,10 @@ import {
   View
 } from 'react-native';
 
-export default class SuperExample extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import app from './app';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const index = () => <App />;
 
-AppRegistry.registerComponent('SuperExample', () => SuperExample);
+export default index;
+
+AppRegistry.registerComponent('SuperExample', () => index);
--- SuperExample/index.ios.js
+++ SuperExample/index.ios.js
@@ -12,42 +12,10 @@ import {
   View
 } from 'react-native';
 
-export default class SuperExample extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import app from './app';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const index = () => <App />;
 
-AppRegistry.registerComponent('SuperExample', () => SuperExample);
+export default index;
+
+AppRegistry.registerComponent('SuperExample', () => index);
