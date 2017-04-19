eaa23a3fb44b9739645957d4566694360de297e9
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-19 19:54:17 +1000

    Basic navigation

--- SuperExample/app.js
+++ SuperExample/app.js
@@ -1,42 +1,12 @@
-import React from 'react';
 import {
-  View,
-  Text,
   AppRegistry,
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-};
+import Home from './screens/Home';
 
-const app = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Welcome to React Native!
-    </Text>
-    <Text style={styles.instructions}>
-      To get started, edit app.js
-    </Text>
-    <Text style={styles.instructions}>
-      Double tap R on your keyboard to reload,{'\n'}
-      Shake or press menu button for dev menu
-    </Text>
-  </View>
-);
+const app = StackNavigator({
+  Home: { screen: Home },
+});
 
 AppRegistry.registerComponent('SuperExample', () => app);
