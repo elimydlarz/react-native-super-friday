0bb9508d46d65cd3205202bd5b870d7e78ca95e0
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-19 22:47:46 +1000

    Swiping

--- SuperExample/screens/Form.js
+++ SuperExample/screens/Form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, View, Button } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 import Field from '../components/Field';
 
@@ -23,44 +24,48 @@ export default class extends Component {
   render() {
     return (
       <ScrollView>
-        <Text style={{ fontSize: 20, padding: 10 }}>
-          {this.nameEntered()
-            ? `Hello ${this.state.name}!`
-            : 'Hey there Chump!'}
-        </Text>
-        <Field
-          label="Please enter your name"
-          updater={value => this.setState({ name: value })}
-          value={this.state.name}
-        />
-        <Field
-          inputStyle={{ borderColor: 'red' }}
-          label="Please enter your number"
-          updater={value => this.setState({ number: value })}
-          value={this.state.number}
-        />
-        <View style={{ flex: 100, flexDirection: 'row', justifyContent: 'space-between', flexWrap: 'wrap' }}>
-          <Button
-            style={{ flex: 50 }}
-            title="Fairly Big Button"
-            onPress={() => {}}
-          />
-          <Button
-            style={{ flex: 50 }}
-            title="Another Big Button"
-            onPress={() => {}}
-          />
-          <Button
-            style={{ flex: 50 }}
-            title="Yet Another Big Button"
-            onPress={() => {}}
-          />
-          <Button
-            style={{ flex: 50 }}
-            title="Final Big Button"
-            onPress={() => {}}
-          />
-        </View>
+        <Swiper height={500}>
+          <View>
+            <Text style={{ fontSize: 20, padding: 10 }}>
+              {this.nameEntered()
+                ? `Hello ${this.state.name}!`
+                : 'Hey there Chump!'}
+            </Text>
+            <Field
+              label="Please enter your name"
+              updater={value => this.setState({ name: value })}
+              value={this.state.name}
+            />
+            <Field
+              inputStyle={{ borderColor: 'red' }}
+              label="Please enter your number"
+              updater={value => this.setState({ number: value })}
+              value={this.state.number}
+            />
+          </View>
+          <View style={{ flex: 100, flexDirection: 'row', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+            <Button
+              style={{ flex: 50 }}
+              title="Fairly Big Button"
+              onPress={() => {}}
+            />
+            <Button
+              style={{ flex: 50 }}
+              title="Another Big Button"
+              onPress={() => {}}
+            />
+            <Button
+              style={{ flex: 50 }}
+              title="Yet Another Big Button"
+              onPress={() => {}}
+            />
+            <Button
+              style={{ flex: 50 }}
+              title="Final Big Button"
+              onPress={() => {}}
+            />
+          </View>
+        </Swiper>
       </ScrollView>
     );
   }
