97cb94dc151bf58edf5df21b623bc051bb1f36c0
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-19 19:26:29 +1000

    linting

--- SuperExample/index.android.js
+++ SuperExample/index.android.js
@@ -4,12 +4,9 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
 
 import App from './app';
