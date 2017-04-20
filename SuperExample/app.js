b72ed370bae037a1173ccc05306e5342bcc047ab
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-20 18:37:01 +1000

    navigation

--- SuperExample/app.js
+++ SuperExample/app.js
@@ -1,43 +1,14 @@
 import React from 'react';
 import {
   AppRegistry,
-  Text,
-  View
 } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-};
+import Home from './screens/Home';
 
-const app = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>
-      Welcome to React Native!
-    </Text>
-    <Text style={styles.instructions}>
-      To get started, edit index.android.js
-    </Text>
-    <Text style={styles.instructions}>
-      Double tap R on your keyboard to reload,{'\n'}
-      Shake or press menu button for dev menu
-    </Text>
-  </View>
-);
+const app = StackNavigator({
+  Home: { screen: Home },
+});
 
 export default app;
 
