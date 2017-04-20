43b6d066ebf20261f6f5113ae94261d928cfdf5e
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-20 19:11:30 +1000

    gallery

--- SuperExample/screens/Gallery.js
+++ SuperExample/screens/Gallery.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import {
-  Text,
+  View,
+  Image,
 } from 'react-native';
 
+import photo from '../images/undertaker.jpg';
+
 const component = () => (
-  <Text>Sup</Text>
+  <View
+    style={{
+      padding: 10,
+      flex: 9,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+    }}
+  >
+    <Image
+      style={{ width: 100, height: 100, margin: 5 }}
+      resizeMode="cover"
+      source={photo}
+    />
+    <Image
+      style={{ width: 100, height: 100, margin: 5 }}
+      resizeMode="cover"
+      source={photo}
+    />
+    <Image
+      style={{ width: 100, height: 100, margin: 5 }}
+      resizeMode="cover"
+      source={photo}
+    />
+    <Image
+      style={{ width: 100, height: 100, margin: 5 }}
+      resizeMode="cover"
+      source={photo}
+    />
+  </View>
 );
 
 component.navigationOptions = { title: 'Gallery' };
